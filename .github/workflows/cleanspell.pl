7725ac2d50fd109e34cf78539ea7f71ca58c689c
Daniel Stenberg <daniel@haxx.se>
2022-09-17 22:53:23 +0200

    cleanspell: strip more curl words

--- .github/workflows/cleanspell.pl
+++ .github/workflows/cleanspell.pl
@@ -24,19 +24,28 @@ while(<F>) {
     elsif(!$ignore) {
         # filter out mentioned CURLE_ names
         $_ =~ s/CURLE_[A-Z0-9_]*//g;
+        $_ =~ s/CURLPX_[A-Z0-9_]*//g;
         $_ =~ s/CURLOPT_[A-Z0-9_]*//g;
         $_ =~ s/CURLMOPT_[A-Z0-9_]*//g;
         $_ =~ s/CURL_DISABLE_[A-Z0-9_]*//g;
         $_ =~ s/CURL_RTSPREQ_[A-Z0-9_]*//g;
         $_ =~ s/CURL_TIMECOND_[A-Z0-9_]*//g;
         $_ =~ s/CURL_IPRESOLVE_[A-Z0-9_]*//g;
+        $_ =~ s/CURL_READFUNC_[A-Z0-9_]*//g;
+        $_ =~ s/CURL_REDIR_[A-Z0-9_]*//g;
         $_ =~ s/CURLPIPE_[A-Z0-9_]*//g;
         $_ =~ s/CURLSHOPT_[A-Z0-9_]*//g;
         $_ =~ s/CURLAUTH_[A-Z0-9_]*//g;
         $_ =~ s/CURLALTSVC_[A-Z0-9_]*//g;
+        $_ =~ s/CURLSSH_[A-Z0-9_]*//g;
         $_ =~ s/CURLFTP_[A-Z0-9_]*//g;
+        $_ =~ s/CURLPROTO_[A-Z0-9_]*//g;
+        $_ =~ s/CURLFTPAUTH_[A-Z0-9_]*//g;
+        $_ =~ s/CURLFTPMETHOD_[A-Z0-9_]*//g;
         $_ =~ s/CURLGSSAPI_[A-Z0-9_]*//g;
         $_ =~ s/CURLMIMEOPT_[A-Z0-9_]*//g;
+        $_ =~ s/CURLSSLBACKEND_[A-Z0-9_]*//g;
+        $_ =~ s/CURL_PROGRESSFUNC_CONTINUE//g;
         print O $_;
     }
 }
